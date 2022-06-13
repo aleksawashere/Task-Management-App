870d861c951dc8b08decd9de2a1b9c5968b8f335
aleksawashere <aleksa.dimitrijevic.business@gmail.com>
2022-06-13 22:54:40 +0200

    Added Tasks functionality

--- Task Management App/View/Home.swift
+++ Task Management App/View/Home.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Home: View {
+    @StateObject var taskModel: TaskViewModel = TaskViewModel()
+    @Namespace var animation
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
 
@@ -18,10 +20,58 @@ struct Home: View {
                 Section{
                 //MARK: Current Week View
                     ScrollView(.horizontal, showsIndicators: false){
+                        
                         HStack(spacing:10){
-                            
+                            ForEach(taskModel.currentWeek, id: \.self){ day in
+                                
+                                VStack(spacing:10){
+                                    
+                                    Text(taskModel.extractDate(date: day, format: "dd"))
+                                        .font(.system(size:14))
+                                        .fontWeight(.semibold)
+
+                                    
+                                    //EEE will return day in as MON, TUE,...
+                                    Text(taskModel.extractDate(date: day, format: "EEE"))
+                                        .font(.system(size:14))
+                                    
+                                    Circle()
+                                        .fill(.white)
+                                        .frame(width:8, height: 8)
+                                        .opacity(taskModel.isToday(date: day) ? 1 : 0)
+                                }
+                                //MARK: Foreground Style
+                                .foregroundStyle(taskModel.isToday(date: day) ? .primary : .secondary)
+                                .foregroundColor(taskModel.isToday(date: day) ? .white : .black)
+                                //MARK: Capsule shape
+                                .frame(width: 45, height: 90)
+                                .background(
+                                
+                                    ZStack{
+                                        //MARK: Animation when we change weekday
+                                        if taskModel.isToday(date: day){
+                                            Capsule()
+                                                .fill(.orange)
+                                                .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
+                                        }
+                                        
+                                    }
+                                    
+                                )
+                                .contentShape(Capsule())
+                                .onTapGesture {
+                                    withAnimation{
+                                        taskModel.currentDay = day
+                                    }
+                                }
+                                
+                                
+                                
+                            }
                         }
+                        .padding(.horizontal)
                     }
+                    TasksView()
                     
                 } header: {
                     HeaderView()
@@ -32,6 +82,90 @@ struct Home: View {
         }
 
     }
+    
+    //MARK: Tasks View
+    func TasksView()->some View{
+        LazyVStack(spacing:18){
+            if let tasks = taskModel.filteredTasks{
+                if tasks.isEmpty{
+                    Text("Uživaj u ostatku dana!☀️")
+                        .font(.system(size:16))
+                        .fontWeight(.light)
+                    Text("Nema više zadataka za tebe danas!🎉")
+                        .font(.system(size:16))
+                        .fontWeight(.light)
+                }
+                else{
+                    ForEach(tasks){ task in
+                        TaskCardView(task: task)
+                    }
+                }
+            }
+            else{
+                //MARK: Progress View
+                ProgressView()
+                    .offset(y: 100)
+            }
+        }
+        .padding()
+        .padding(.top)
+        //MARK: Updating
+        .onChange(of: taskModel.currentDay) { newValue in
+            taskModel.filterTodaysTasks()
+        }
+        
+    }
+    
+    
+    //MARK: Task Card View
+    func TaskCardView(task: Task)->some View{
+        HStack(alignment: .top, spacing:30){
+            VStack(spacing:10){
+                Circle()
+                    .fill(Color("OrangeTick"))
+                    .frame(width: 15, height: 15)
+                    .background(
+                    Circle()
+                        .stroke(Color("OrangeTick"),lineWidth: 1)
+                        .padding(-3)
+                    )
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width:3)
+            }
+            
+            VStack{
+                HStack(alignment: .top, spacing: 10){
+                    
+                    VStack(alignment: .leading, spacing: 12){
+                        
+                        Text(task.taskTitle)
+                            .font(.title2.bold())
+                            
+                        Text(task.taskDescription)
+                            .font(.callout)
+                            
+                    }
+                    .hLeading()
+                    
+                    Text(task.taskDate.formatted(date:.omitted, time:.shortened))
+                }
+                
+            }
+            .foregroundColor(.white)
+            .padding()
+            .hLeading()
+            .background(
+                Color("OrangeTick")
+                    .cornerRadius(25)
+            
+            )
+        }
+        .hLeading()
+    }
+    
+    
+    
     
     //MARK: Header
     func HeaderView()->some View{
@@ -68,6 +202,7 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+            .previewInterfaceOrientation(.portrait)
     }
 }
 
--- Task Management App/ViewModel/TaskViewModel.swift
+++ Task Management App/ViewModel/TaskViewModel.swift
@@ -12,16 +12,81 @@ class TaskViewModel: ObservableObject{
     
     @Published var storedTasks: [Task] = [
     
-        Task(taskTitle: "Zavrsi projekat", taskDescription: "Sredi dizajn i funkcionalnosti na samom projektu", taskDate: .init(timeIntervalSince1970: 1641645487)),
-        Task(taskTitle: "Nauci osnove kvaliteta za ispit", taskDescription: "Vidi gradivo koje je potrebno da spremis", taskDate: .init(timeIntervalSince1970: 1641645487)),
-        Task(taskTitle: "Pregledaj dizajn aplikacije", taskDescription: "Dodatno prepravi ako se nesto ne uklapa", taskDate: .init(timeIntervalSince1970: 1641645487)),
-        Task(taskTitle: "Prijavi temu diplomskog rada", taskDescription: "U studentskoj sluzbi prijavi i sacekaj proceduru", taskDate: .init(timeIntervalSince1970: 1641645487)),
-        Task(taskTitle: "Zavrsi igricu u Unity okruzenju", taskDescription: "Dodaj funkcionalnosti za glavnog junaka", taskDate: .init(timeIntervalSince1970: 1641645487)),
-        Task(taskTitle: "Trening", taskDescription: "Ruke i ramena", taskDate: .init(timeIntervalSince1970: 1641645487)),
-    
-    
-    
-    
-    
+        Task(taskTitle: "Zavrsi projekat", taskDescription: "Sredi dizajn i funkcionalnosti na samom projektu", taskDate: .init(timeIntervalSince1970: 1655144826)),
+        Task(taskTitle: "Nauci osnove kvaliteta za ispit", taskDescription: "Vidi gradivo koje je potrebno da spremis", taskDate: .init(timeIntervalSince1970: 1655144826)),
+        Task(taskTitle: "Pregledaj dizajn aplikacije", taskDescription: "Dodatno prepravi ako se nesto ne uklapa", taskDate: .init(timeIntervalSince1970: 1655200911)),
+        Task(taskTitle: "Prijavi temu diplomskog rada", taskDescription: "U studentskoj sluzbi prijavi i sacekaj proceduru", taskDate: .init(timeIntervalSince1970: 1655200911)),
+        Task(taskTitle: "Zavrsi igricu u Unity okruzenju", taskDescription: "Dodaj funkcionalnosti za glavnog junaka", taskDate: .init(timeIntervalSince1970: 1655200911)),
+        Task(taskTitle: "Trening", taskDescription: "Ruke i ramena", taskDate: .init(timeIntervalSince1970: 1655200911))
     ]
+    
+    //MARK: Current Week Days
+    
+    @Published var currentWeek: [Date] = []
+    
+    //MARK: Current Day
+    //Pamcenjem ove varijable, znacemo na kom danu se nalazimo kada korisnik klikne
+    @Published var currentDay: Date = Date()
+    
+    //MARK: Filtering todays tasks
+    
+    @Published var filteredTasks: [Task]?
+    
+    //MARK: Initializing
+    init(){
+        fetchCurrentWeek()
+        filterTodaysTasks()
+    }
+    
+    //MARK: Filter Todays Tasks
+    
+    func filterTodaysTasks(){
+        DispatchQueue.global(qos: .userInteractive).async {
+            
+            let calendar = Calendar.current
+            
+            let filtered = self.storedTasks.filter{
+                return calendar.isDate($0.taskDate, inSameDayAs: self.currentDay)
+            }
+            
+            DispatchQueue.main.async {
+                withAnimation{
+                    self.filteredTasks = filtered
+                }
+            }
+        }
+    }
+    
+    func fetchCurrentWeek(){
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let week = calendar.dateInterval(of: .weekOfMonth, for: today)
+        
+        guard let firstWeekDay = week?.start else{
+            return
+        }
+        
+        (1...7).forEach { day in
+           if let weekday = calendar.date(byAdding: .day, value: day, to: firstWeekDay){
+                currentWeek.append(weekday)
+            }
+        }
+    }
+    
+    //MARK: Extracting Date
+    
+    func extractDate(date: Date, format: String) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
+    
+    //MARK: Checking if current Date is Today
+    //Moramo proveriti svakog puta koji dan je Danas, kako bi se na taj element prvo "zakacili"
+    
+    func isToday(date: Date)->Bool{
+        let calender = Calendar.current
+        return calender.isDate(currentDay, inSameDayAs: date)
+    }
 }
